ec504ddb2ed6e0b8d471fc271537eba9d386b9c6
shihanze <shihanmails@gmail.com>
2024-09-04 16:22:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int number = 6;
+
+for (int i = 1; i <= number; i++)
+{
+    // Print leading spaces
+    for (int j = 1; j <= number - i; j++)
+    {
+        Console.Write(" ");
+    }
+
+    // Print the "A" characters
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("A ");
+    }
+
+    // Move to the next line
+    Console.WriteLine();
+}
